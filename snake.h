4e33bbf8a7788b31e854f01c9bcf2df77210b550
Job <113185077+webnefor@users.noreply.github.com>
2024-04-18 15:59:41 +0300

    Update snake.h

--- snake.h
+++ snake.h
@@ -21,7 +21,6 @@ int c_kbhit(void);
 
 
 int tail(int *position_snake_x, int i, int j, int *position_snake_y, int decrement_x);
-int  snake_tail();
 int  snake(struct xyPlace *p_xy);
 int  input(struct xyPlace *p_xy);
 void applePosition(int *value_position_fruit);
